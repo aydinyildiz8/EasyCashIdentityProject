5cd1a6732f723b60d0a29ffb2bfc4600415efb6e
Aydın Yıldız <113350420+aydinyildiz8@users.noreply.github.com>
2023-05-02 22:45:58 +0300

    4.Ders Sonu

---
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccountProcess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    public class CustomerAccountProcess
+    {
+        public int CustomerAccountProcessID { get; set; }
+        public string CustomerAccountProcessType { get;set; }
+        public decimal CustomerAccountProcessAmount { get;set; }
+        public DateTime CustomerAccountProcessDate{ get;set; }
+    }
+}
