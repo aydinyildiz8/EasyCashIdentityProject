44ab76eac36ef2a4fffbd5ca0dd6ba270d6a4390
Aydın Yıldız <113350420+aydinyildiz8@users.noreply.github.com>
2023-05-15 20:54:53 +0300

    13.Ders Sonu

--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -1,7 +1,9 @@
 ﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
 using EasyCashIdentityProject.EntityLayer.Concrete;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
 
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
@@ -25,6 +27,9 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
         {
             if (ModelState.IsValid)
             {
+                Random random = new Random();
+                int code = random.Next(100000, 1000000);
+
                 AppUser appUser = new AppUser()
                 {
                     UserName = appUserRegisterDto.Username,
@@ -33,13 +38,36 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
                     Email = appUserRegisterDto.Email,
                     City = "aa",
                     District = "BB",
-                    ImageUrl = "cc"
+                    ImageUrl = "cc",
+                    ConfirmCode = code
+
 
                 };
                 var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
 
                 if (result.Succeeded)
                 {
+                    MimeMessage mimeMessage = new MimeMessage();
+                    MailboxAddress mailboxAddressFrom = new MailboxAddress("Admin", "aydnyldz8@gmail.com");
+                    MailboxAddress mailboxAddressTo = new MailboxAddress("User", appUser.Email);
+
+                    mimeMessage.From.Add(mailboxAddressFrom);
+                    mimeMessage.To.Add(mailboxAddressTo);
+
+                    var bodyBuilder = new BodyBuilder();
+                    bodyBuilder.TextBody = "Kayıt işlemini tamamlamak için onay kodunuz: " + code;
+
+                    mimeMessage.Body = bodyBuilder.ToMessageBody();
+                    mimeMessage.Subject = "Easy Cash Onay Kodu";
+
+                    SmtpClient client = new SmtpClient();
+                    client.Connect("smtp.gmail.com", 587, false);
+                    client.Authenticate("aydnyldz8@gmail.com", "kccxlerutthfyuol");
+                    client.Send(mimeMessage);
+                    client.Disconnect(true);
+
+
+
                     return RedirectToAction("Index", "ConfirmMail");
                 }
                 else
